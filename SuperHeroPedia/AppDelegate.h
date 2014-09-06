dc157e2034c3810b8a1a36f77abcd8c0bb1c2306
Dave Krawczyk <dave.krawczyk@gmail.com>
2014-09-06 13:25:01 -0500

    Initial Commit

---
+++ SuperHeroPedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperHeroPedia
+//
+//  Created by Dave Krawczyk on 9/6/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperHeroPedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperHeroPedia
+//
+//  Created by Dave Krawczyk on 9/6/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
